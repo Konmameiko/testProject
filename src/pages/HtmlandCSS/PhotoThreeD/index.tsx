46b07ba7bad006ab4b4fd135d7641b6b977465d4
konmameiko <a13224010118@163.com>
2021-08-14 23:40:05 +0800

    20210814

---
+++ src/pages/HtmlandCSS/PhotoThreeD/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './index.less'
+
+function index() {
+    return (
+        <div className={styles.simulateBody}>
+            <div className={styles.outer}>
+                <div className={styles.photos}>
+                    <div className={styles.photo1}></div>
+                    <div className={styles.photo2}></div>
+                    <div className={styles.photo3}></div>
+                    <div className={styles.photo4}></div>
+                    {/* <div className={styles.photo1}></div> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default index
+
