4e7939c8205e8271a0fdd176f37f7b687225539d
konmameiko <a13224010118@163.com>
2021-08-15 01:43:19 +0800

    "20210815 SSH测试"

--- src/pages/HtmlandCSS/TimeShaftMake/index.tsx
+++ src/pages/HtmlandCSS/TimeShaftMake/index.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import styles from './index.less'
 
 export default class index extends Component {
     render() {
         return (
-            <div>
-                hello world!
+            <div className={styles.simulateBody}>
+                
             </div>
         )
     }
