2d517b9c227c5881bbe330afa897abdae35812e3
konmameiko <a13224010118@163.com>
2021-08-30 01:13:50 +0800

    20210830

--- src/pages/HtmlandCSS/simpleLogin/index.tsx
+++ src/pages/HtmlandCSS/simpleLogin/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import styles from './index'
+import styles from './index.less'
 
 function index() {
     return (
-        <div>
-            
+        <div className={styles.body}>
+            <div className={styles.text}>
+                <p className={styles.name1}>简易</p>
+                <p className={styles.name2}>的</p>
+                <p className={styles.name3}>Login</p>
+            </div>
+            <div className={styles.a}>
+                <div className={styles.b}>
+                    <p className={styles.c}>Login</p>
+                    <div className={styles.d}>
+                        <input type="text" className={styles.e} placeholder='账号' />
+                        <input type="password" className={styles.e} placeholder='密码' />
+                        <div className={styles.f}>Go</div>
+                    </div>
+                    <div className={styles.g}>Forget the Password?<a href='#'>Retrieve</a></div>
+                </div>
+            </div>
         </div>
     )
 }
