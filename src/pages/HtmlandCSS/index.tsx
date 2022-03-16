3448a0e5721df9c735f28d73a20cb323fecb37a0
konmameiko <a13224010118@163.com>
2022-03-16 22:46:43 +0800

    fix switch Bug

--- src/pages/HtmlandCSS/index.tsx
+++ src/pages/HtmlandCSS/index.tsx
@@ -29,23 +29,23 @@ class index extends Component {
         key : sessionStorage.getItem('HtmlDemoIndex') || '0'
     }
     array = [
-        {name:"图片立方体旋转",index:"1"},
-        {name:"轮播图与滚动图片",index:"2"},
-        {name:"图片擦除",index:"3"},
-        {name:"图片3D 轮播",index:"4"},
-        {name:"时间轴制作",index:"5"},
-        {name:"简易搜索框",index:"6"},
-        {name:"简易登陆界面",index:"7"},
-        {name:"拖拽及缩放页面",index:"8"},
-        {name:"平缓的缓冲效果",index:"9"},
-        {name:"拖拽生成字符串",index:"10"},
-        {name:"雪花特效",index:"11"},
-        {name:"海浪动画",index:"12"},
-        {name:"简单的视差滚动效果",index:"13"},
-        {name:"星际穿越",index:"14"},
-        {name:"临时Demo页",index:"15"},
-        {name:"毛玻璃效果",index:"16"},
-        {name:"毛玻璃渐变背景",index:"17"},
+        {name:"图片立方体旋转",index:"1", component: <Rotate/>},
+        {name:"轮播图与滚动图片",index:"2", component: <Sliding/>},
+        {name:"图片擦除",index:"3", component: <Erasure/>},
+        {name:"图片3D 轮播",index:"4", component: <ThreeD/>},
+        {name:"时间轴制作",index:"5", component: <TimerShaft/>},
+        {name:"简易搜索框",index:"6", component: <SimpleSearch/>},
+        {name:"简易登陆界面",index:"7", component: <SimpleLogin/>},
+        {name:"拖拽缩放页面",index:"8", component: <Drop/>},
+        {name:"load组件页",index:"9", component: <FlatPreloader/>},
+        {name:"拖拽生成字符串",index:"10", component: <DrawText/>},
+        {name:"雪花特效",index:"11", component: <StarSky/>},
+        {name:"海浪动画",index:"12", component: <Wave/>},
+        {name:"简单的视差滚动效果",index:"13", component: <NotFound/>},
+        {name:"星际穿越",index:"14", component: <Interstellar/>},
+        {name:"临时Demo页",index:"15", component: <Temporary/>},
+        {name:"毛玻璃效果",index:"16", component: <FrostedGlass/>},
+        {name:"毛玻璃渐变背景",index:"17", component: <FrostedGlassBg/>},
     ];
 
     //获取url中的参数方法
@@ -64,42 +64,7 @@ class index extends Component {
     };
 
     switchDemo = (value:any) =>{
-        switch(Number(value)+1){
-            case 1:
-                return <Rotate/>
-            case 2:
-                return <Sliding/>
-            case 3:
-                return <Erasure/>
-            case 4:
-                return <ThreeD/>
-            case 5:
-                return <TimerShaft/>
-            case 6:
-                return <SimpleSearch/>
-            case 7:
-                return <SimpleLogin/>
-            case 8:
-                return <Drop/>
-            case 9:
-                return <FlatPreloader/>
-            case 10:
-                return <DrawText/>
-            case 11:
-                return <StarSky/>
-            case 12:
-                return <Wave/>
-            case 13:
-                return <NotFound/>
-            case 14:
-                return <Interstellar/>
-            case 15:
-                return <Temporary/>
-            case 16:
-                return <FrostedGlass/>
-            case 17:
-                return <FrostedGlassBg/>
-        }
+        return this.array[value].component;
     }
 
     setTabKey = (key:any) =>{
