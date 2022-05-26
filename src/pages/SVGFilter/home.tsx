98a51036baa312f97207da8c52e24453a97f3970
konmameiko <a13224010118@163.com>
2022-05-26 22:58:17 +0800

    更新所有依赖项

--- src/pages/SVGFilter/home.tsx
+++ src/pages/SVGFilter/home.tsx
@@ -2,47 +2,53 @@
  * @Description: SVG滤镜
  * @Author: KonmaMeiko
  * @Date: 2022-05-09 14:52:03
- * @LastEditTime: 2022-05-22 19:46:31
+ * @LastEditTime: 2022-05-26 22:55:59
  * @LastEditors: KonmaMeiko
  */
-import React, {FC, useCallback, useEffect, useRef, useState} from 'react';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { Layout, Menu } from 'antd';
 import styles from './home.scss';
 export interface prop {}
 
 const { Header, Sider, Content } = Layout;
-interface IProps extends Partial<prop>{
-    onSomething: ()=>void;
+interface IProps extends Partial<prop> {
+  onSomething: () => void;
 }
 
-const SVGFilter:FC<IProps> = ({onSomething})=>{
+const SVGFilter: FC<IProps> = ({ onSomething }) => {
+  const handleChange = (item: any) => {
+    console.log(item);
+  };
 
-    const handleChange = (item:any) =>{
-        console.log(item);
-    }
+  const items = [
+    { label: '菜单项一', key: 'item-1' }, // 菜单项务必填写 key
+    { label: '菜单项二', key: 'item-2' },
+    {
+      label: '子菜单',
+      key: 'submenu',
+      children: [{ label: '子菜单项', key: 'submenu-item-1' }],
+    },
+  ];
 
-    return (
-        <Layout className={styles.home}>
-            <Sider trigger={null} collapsible>
-                <div className={styles.logo} />
-                <Menu onClick={(item)=>handleChange(item)}>
-                    <Menu.Item>菜单项一</Menu.Item>
-                    <Menu.Item>菜单项二</Menu.Item>
-                </Menu>;
-            </Sider>
-            <Layout className={styles.siteLayout}>
-                <Content
-                    className={styles.siteLayoutBackground}
-                    style={{
-                    padding: 24,
-                    minHeight: '100vh',
-                    }}
-                >
-                    Content
-                </Content>
-            </Layout>
-        </Layout>
-    );
-}
+  return (
+    <Layout className={styles.home}>
+      <Sider trigger={null} collapsible>
+        <div className={styles.logo} />
+        <Menu onClick={(item) => handleChange(item)} items={items} />
+      </Sider>
+      <Layout className={styles.siteLayout}>
+        <Content
+          className={styles.siteLayoutBackground}
+          style={{
+            padding: 24,
+            minHeight: '100vh',
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 //memo的使用在不希望一直跟随父组件更新的子组件上
 export default React.memo(SVGFilter);
