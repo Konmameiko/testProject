012ef071eaab0e86972c9a7dad59e89ef3c1663b
konmameiko <a13224010118@163.com>
2022-06-23 22:18:53 +0800

    添加音频导入

--- src/pages/HtmlandCSS/photoMoveMode/index.tsx
+++ src/pages/HtmlandCSS/photoMoveMode/index.tsx
@@ -2,7 +2,7 @@
  * @Description: 照片选取页
  * @Author: ly-yuzh
  * @Date: 2022-06-20 17:43:18
- * @LastEditTime: 2022-06-22 22:58:54
+ * @LastEditTime: 2022-06-23 22:17:11
  * @LastEditors: KonmaMeiko
  */
 import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
@@ -14,12 +14,19 @@ import image3 from '@/assets/images/3.jpg';
 import image4 from '@/assets/images/4.jpg';
 import image5 from '@/assets/images/5.jpg';
 
+import voice1 from '@/assets/audios/windy.mp3';
 export interface IProps {}
 
 const PhotoMode: FC<IProps> = () => {
   const [curWidth, setCurWidth] = useState<string>('30%');
-  const [photoWidth, setPhotoWidth] = useState<number>(456);
+  const [photoWidth, setPhotoWidth] = useState<number>(0);
   const bodyref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setCurWidth('50%');
+    setPhotoWidth(document.body.clientWidth * 0.5);
+  }, []);
+
   const handleClick = () => {
     if (curWidth === '80%') {
       setPhotoWidth(document.body.clientWidth * 0.5);
@@ -39,27 +46,45 @@ const PhotoMode: FC<IProps> = () => {
       onClick={() => handleClick()}
     >
       <ul style={{ '--s': 5, '--w': photoWidth, '--speed': '3.5s' }}>
-        <li>
-          <img src={image1} alt="" />
-        </li>
-        <li>
-          <img src={image2} alt="" />
-        </li>
-        <li>
-          <img src={image3} alt="" />
-        </li>
-        <li>
-          <img src={image4} alt="" />
-        </li>
-        <li>
-          <img src={image5} alt="" />
-        </li>
+        <LiItem image={image1} text={'天姥连天向天横'} />
+        <LiItem image={image2} text={'势拔五岳掩赤城'} />
+        <LiItem image={image3} text={'天台四万八千丈'} />
+        <LiItem image={image4} text={'对此欲倒东南倾'} />
+        <LiItem image={image5} text={'我欲因之梦吴越'} />
         {/* 防止最后空缺 填补首图 */}
-        <li>
-          <img src={image1} alt="" />
-        </li>
+        <LiItem image={image1} text={'天姥连天向天横'} />
       </ul>
+      <AudioPlay />
     </div>
+  );
+};
+
+const AudioPlay = () => {
+  return (
+    <audio
+      style={{ position: 'absolute', top: 0 }}
+      src={voice1} // 引入本地音频
+      autoPlay
+      // loop
+      // controls={true}
+      onEnded={() => {
+        console.log('what fuck');
+      }}
+      crossOrigin="anonymous"
+    />
+  );
+};
+interface IliProps {
+  image: string;
+  text: string;
+}
+const LiItem = (props: IliProps) => {
+  const { image, text } = props;
+  return (
+    <li>
+      <img src={image} alt="" />
+      <p>{text}</p>
+    </li>
   );
 };
 //memo的使用在不希望一直跟随父组件更新的子组件上
