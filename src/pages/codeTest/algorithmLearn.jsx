c14c8cce9199ec77b1d3fc14c4c8dfe0494e8839
konmameiko <a13224010118@163.com>
2022-10-08 15:51:46 +0800

    fix: 本日算法代码测试

--- src/pages/codeTest/algorithmLearn.jsx
+++ src/pages/codeTest/algorithmLearn.jsx
@@ -1,21 +1,32 @@
+/*
+ * @Description:
+ * @Author: KonmaMeiko
+ * @Date: 2022-07-30 17:46:09
+ * @lastEditTime: Do not edit
+ * @LastEditors: KonmaMeiko
+ */
 import React from 'react';
 const ProgressDemos = () => {
   // let x = 2;
   // let n = 10;
-  var myPow = (x, n) => {
-    console.log(x, n);
-    if (n == 0 || n == 1) {
-      return n == 0 ? 1 : x;
-    } else if (n < 0) {
-      return myPow(1 / x, Math.abs(n));
-    } else {
-      return n % 2 == 0
-        ? myPow(x * x, n / 2)
-        : myPow(x * x, Math.floor(n / 2)) * x;
+  var myPow = (nums1, nums2) => {
+    const n = nums1.length;
+    const idx = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) idx[i] = i;
+    nums1.sort((a, b) => a - b);
+    idx.sort((i, j) => nums2[i] - nums2[j]);
+    let L = 0,
+      R = n - 1; // nums2(索引)的左右指针
+    for (let i of nums1) {
+      console.log(i, nums2[idx[L]], idx);
+      if (i > nums2[idx[L]])
+        nums2[idx[L++]] = i; // 可以满足 nums1[i] > nums2[i]
+      else nums2[idx[R--]] = i; //丢到数组最后
     }
+    return nums2;
   };
   const handleClick = () => {
-    return myPow(2, 10);
+    return myPow([2, 7, 11, 15], [1, 9, 4, 11]);
   };
 
   return (
