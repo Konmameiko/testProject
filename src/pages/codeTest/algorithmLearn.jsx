0ca8442f02aad78e2a45b2ecc11e2a1934dc4d9c
konmameiko <a13224010118@163.com>
2023-03-13 23:38:37 +0800

    [MODIFY]:修改首页文字 [Author]:Konmameiko

--- src/pages/codeTest/algorithmLearn.jsx
+++ src/pages/codeTest/algorithmLearn.jsx
@@ -10,30 +10,44 @@ import { TimePicker } from 'antd';
 import moment from 'moment';
 
 const ProgressDemos = () => {
-  // 解压缩字符串 '2<A>' AA
-  // 'A2<B2<CE>>' ABCECEBCECE
-  var myPow = (str) => {
-    const stack = [];
-    let res = '';
-    let num = 0;
-    for (let c of str) {
-      if (c === '[') {
-        stack.push([num, res]);
-        [num, res] = [0, ''];
-      } else if (c === ']') {
-        let [preNum, preRes] = stack.pop();
-        res = preRes + res.repeat(preNum);
-      } else if (!isNaN(c)) {
-        num = num * 10 + parseInt(c);
+  var minNumberOfHours = function (
+    initialEnergy,
+    initialExperience,
+    energy,
+    experience,
+  ) {
+    let sum = 0;
+    for (const e of energy) {
+      sum += e;
+    }
+    let trainingHours = initialEnergy > sum ? 0 : sum + 1 - initialEnergy;
+    console.log(trainingHours);
+    for (const e of experience) {
+      if (initialExperience <= e) {
+        trainingHours += 1 + (e - initialExperience);
+        initialExperience = 2 * e + 1;
       } else {
-        res += c;
+        initialExperience += e;
       }
     }
-    return res;
+    return trainingHours;
   };
   const handleClick = () => {
     console.time();
-    const res = myPow('A2<B2<CE>>');
+    const res = minNumberOfHours(
+      11,
+      23,
+      [
+        69, 22, 93, 68, 57, 76, 54, 72, 8, 78, 88, 15, 58, 61, 25, 70, 58, 91,
+        79, 22, 91, 74, 90, 75, 31, 53, 31, 7, 51, 96, 76, 17, 64, 89, 83, 54,
+        28, 33, 32, 45, 20,
+      ],
+      [
+        51, 81, 46, 80, 56, 7, 46, 74, 64, 20, 84, 66, 13, 91, 49, 30, 75, 43,
+        74, 88, 82, 51, 72, 4, 80, 30, 10, 19, 40, 27, 21, 71, 24, 94, 79, 13,
+        40, 28, 63, 85, 70,
+      ],
+    );
     console.timeEnd();
     return res;
   };
