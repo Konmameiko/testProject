d7a9ba94b2346bd986a6f5892003d4698baed55f
konmameiko <a13224010118@163.com>
2023-04-09 00:36:56 +0800

    [MODIFY]:静态资源页面高度样式调整 [Author]:Konmameiko

--- src/pages/HtmlandCSS/photoMoveMode/index.tsx
+++ src/pages/HtmlandCSS/photoMoveMode/index.tsx
@@ -12,7 +12,6 @@ import image1 from '@/assets/images/1.jpg';
 import image2 from '@/assets/images/2.jpg';
 import image3 from '@/assets/images/3.jpg';
 import image4 from '@/assets/images/4.jpg';
-import image5 from '@/assets/images/5.jpg';
 import image6 from '@/assets/images/12.gif';
 
 import voice1 from '@/assets/audios/windy.mp3';
