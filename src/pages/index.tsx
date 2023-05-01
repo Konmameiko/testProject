c1dba1569fc86d40773749d61cba48ae098f456e
konmameiko <a13224010118@163.com>
2023-05-02 01:21:30 +0800

    [MODIFY]: 首页路由按钮添加icon [Author]:Konmameiko

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,14 @@
 import styles from './index.less';
 import { Link, history } from 'umi';
 import React, { Component } from 'react';
+import icon1 from '../assets/darksouls_icon/1.png';
+import icon2 from '../assets/darksouls_icon/2.png';
+import icon3 from '../assets/darksouls_icon/3.png';
+import icon4 from '../assets/darksouls_icon/4.png';
+import icon5 from '../assets/darksouls_icon/5.png';
+import icon6 from '../assets/darksouls_icon/6.png';
+import icon7 from '../assets/darksouls_icon/7.png';
+import icon8 from '../assets/darksouls_icon/8.png';
 
 interface menuItemProps {
   text?: string;
@@ -18,13 +26,14 @@ interface menuItemProps {
 export default class IndexPage extends Component<any, any> {
   timer: any | null = null;
   MenuItemArr = [
-    { router: '/photoManage', text: '图片处理', icon: '' },
-    { router: '/Test/Html_CSSLearn', text: '静态页面', icon: '' },
-    { router: '/Test/moreButtons', text: '按钮组件', icon: '' },
-    { router: '/Test/Codes', text: '算法临时', icon: '' },
-    { router: '/Test/text', text: '字体组件', icon: '' },
-    { router: '/Test/svgFilter', text: 'SVG滤镜', icon: '' },
-    { router: '/imgWrapper', text: '图片瀑布流', icon: '' },
+    { router: '/photoManage', text: '图片处理', icon: icon1 },
+    { router: '/Test/Html_CSSLearn', text: '静态页面', icon: icon2 },
+    { router: '/Test/moreButtons', text: '按钮组件', icon: icon3 },
+    { router: '/Test/Codes', text: '算法临时', icon: icon4 },
+    { router: '/Test/text', text: '字体组件', icon: icon5 },
+    { router: '/Test/svgFilter', text: 'SVG滤镜', icon: icon6 },
+    { router: '/imgWrapper', text: '图片瀑布流', icon: icon7 },
+    { router: '/404', text: '', icon: icon8 },
   ];
   componentDidMount() {
     // 关闭首页下方滚动波浪
@@ -60,7 +69,7 @@ export default class IndexPage extends Component<any, any> {
         <div className={styles.glass} />
         <div className={styles.content}>
           <div className={styles.index}>
-            <h1>Hello,world!</h1>
+            <h1>HELLO WORLD</h1>
           </div>
           <div className={styles.menus}>
             {this.MenuItemArr.map((item) => {
