d368b32419b096ccb99711fcf4607f785e87bc56
Konmameiko <a13224010118@163.com>
2024-07-21 00:11:23 +0800

    [FIX]: 图片擦除组件样式优化

--- src/pages/HtmlandCSS/PhotoErasure/index.tsx
+++ src/pages/HtmlandCSS/PhotoErasure/index.tsx
@@ -5,36 +5,27 @@
  * @lastEditTime: Do not edit
  * @LastEditors: KonmaMeiko
  */
-import React, { Component } from 'react';
+import { Component } from 'react';
 import styles from './index.less';
 import Scratch from 'react-scratch-perfect';
 
 class PhotoErasure extends Component {
-	handlePre = (e: any) => {
-		console.log('test');
-	};
-
-	onSuccess = () => {
-		console.log('完成');
-	};
 	render() {
 		return (
 			<div className={styles.body}>
 				<Scratch
 					//填充图片
-					img={require('@/assets/images/8.webp')}
+					img={require('@/assets/images/32.webp')}
 					//内部图片与上下左右的间距值
 					round={[0, 0, 0, 0]}
 					//画笔直径
 					size={50}
 					//完成多少算结束
-					percentage={90}
+					percentage={70}
 					//结束后是否清空画布
 					clear={true}
 					//鼠标抬起时触发事件
 					mode="end"
-					onChange={this.handlePre}
-					onSuccess={this.onSuccess}
 					className={styles.scratch}
 					/* imgRepeat:
                         width: 宽度撑满，高度自适应并居中
@@ -43,7 +34,7 @@ class PhotoErasure extends Component {
                         无值或者其他值会被拉伸铺满容器
                     */
 				>
-					<img className={styles.img2} src={require('@/assets/images/2.webp')} alt="" />
+					<img className={styles.img2} src={require('@/assets/images/8.webp')} alt="" />
 				</Scratch>
 			</div>
 		);
